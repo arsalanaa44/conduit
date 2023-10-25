501e2a65c6d1aaf606d0a90e6c6d41f7d929dee2
mehran mahabadi <mehran.mah44@gmail.com>
2023-10-25 11:59:16 +0330

    add tags table and make many to many relationship with articles table

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -18,6 +18,11 @@ class Article extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
 
 
 }
