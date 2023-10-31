1bad6fddd2124e6ccff0a62cf0a61902260c21a1
mehran mahabadi <mehran.mah44@gmail.com>
2023-10-31 10:36:06 +0330

    add transaceftion model'

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -15,4 +15,9 @@ class Wallet extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
